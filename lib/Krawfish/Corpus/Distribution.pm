5cc0754bb11ed60419d196fee6ba795c4283d823
Akron <nils@diewald-online.de>
2017-08-23 20:05:22 +0200

    Added some notes regarding distribution queries

--- lib/Krawfish/Corpus/Distribution.pm
+++ lib/Krawfish/Corpus/Distribution.pm
@@ -16,11 +16,45 @@ use warnings;
 #   has only a few entries, making most of the buffered elements
 #   rendered useless.
 #   However - the strategy described above means,
-#   that there may be a lot elements missing, so it may be usefull to
+#   that there may be a lot elements missing, so it may be useful to
 #   buffer the query with the lowest freq first and then go through
 #   the other one with mild skips.
 #
 #   However - in case skips are not available,
 #   this may be slow ...
+#
+# TODO:
+#   Another distribution variant would be
+#   distr(doc[1:1]: 'author:Goethe', 'author:Schiller')
+#   where both corpora will have an equal size.
+#   Unfortunately an equal size does not necessarily
+#   mean:
+#     - the text size is equivalent
+#     - the sampling is reasonably good
+#   A solution for the first problem could be to formulate
+#   a query like this:
+#
+#     distr(token[1:1:~10%]: 'author:Goethe', 'author:Schiller')
+#
+#   that would buffer documents from a and be and check the
+#   number of subtokens and always release the buffer, once the
+#   tolerance level is reached (remembering the token difference)
+#   for the buffer. This can be problematic, in case the
+#   tolerance level is never met.
+#   It may also be problematic when one author has - in the result
+#   only one long document and the other author has only 100
+#   short texts.
+#
+#   To deal with the second problem, another query may be provided
+#   as
+#
+#     sampleDistr([2:1]: 'author:Goethe', 'author:Schiller')
+#
+#   where both corpora are first created, based on both sizes
+#   it is chosen which corpus needs to be reduced to reach the
+#   desired ratio, and based on that, the reducible corpus is
+#   sampled (by randomly removing items).
+#   This will be slow!
+
 
 1;
