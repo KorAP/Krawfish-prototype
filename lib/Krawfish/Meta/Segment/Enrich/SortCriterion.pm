a894fe16798bda57d81d603cb0431ebecfe2d3d3
Akron <nils@diewald-online.de>
2017-10-02 22:30:52 +0200

    Added a minor comment to sort criteria
    Change-Id: I997dcfac220d42aae534d7d9a4d745d7ea2e83df

--- lib/Krawfish/Meta/Segment/Enrich/SortCriterion.pm
+++ lib/Krawfish/Meta/Segment/Enrich/SortCriterion.pm
@@ -8,8 +8,16 @@
 # this may be a different API).
 #
 # A proper way to do this would be to go through the sorted
-# lists and create a new sorted list in match order (or to somehow
+# lists and create a new sorted list in doc order (or to somehow
 # keep match order) to make it possible to enrich with all
 # sorting criteria.
-# Otherwise - sometimes the sorting may remember "witnesses" for
-# sorted bundles.
+#
+# 1. For Fields: Create a list of all docs to enrich in doc_id order
+#    (Ignore duplicates)
+# 2. Prepare all requested fields in field order
+# 3. Go through all fields and collect values or term_ids
+# 4. Create criterion vectors per match based on these information
+#
+# On the node level, the relevant criteria (top_k) will be inflated,
+# taken the ordering into account (which mean following matches may
+# have a lot of criteria in common.
