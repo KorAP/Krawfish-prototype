2188920a76f9294f31151715d0803f6babdd812f
Akron <nils@diewald-online.de>
2017-10-25 12:34:28 +0200

    Added a note regarding cursors for paging
    Change-Id: I1874fe4fc933a0bfed88df5202d6a1a0296420ee

---
+++ lib/Krawfish/Compile/Cluster/Cursor.pm
@@ -0,0 +1,9 @@
+# A cursor will enrich the search results by adding a signature
+# of the latest returned match in sorted scenarios. When
+# mirroring this counter, the search will restart at this point
+# in the sorting by adding this value as the top-border in
+# the priority-queue of the segments (meaning this needs to
+# be enabled in the priority queues).
+# Creating the cursor requires an enriching strategy.
+# Supporting cursor in search requires enabling a top-border in
+# the priority queue.
