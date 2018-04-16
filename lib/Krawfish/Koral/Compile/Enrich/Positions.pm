32a4f320148992f9d7b6d5c4023037c768306602
Akron <nils@diewald-online.de>
2018-04-16 14:44:45 +0200

    Added a stub class for concordance plot calculations
    Change-Id: I4ce6878089629986c90a44eba990f204176ad40b

---
+++ lib/Krawfish/Koral/Compile/Enrich/Positions.pm
@@ -0,0 +1,16 @@
+# TODO:
+#   Enrich each document in the result set with
+#   information where in the document a hit occurs.
+#   The positions should be normalized by the length
+#   of the document.
+#   Result may be like:
+#   positions: [
+#     2.4,
+#     8.91,
+#     8.92,
+#     ...
+#     99.5
+#   ]
+#   where each point is a position in a range of 0-100.
+#   This is useful to create ConcordancePlots, see
+#   http://vallandingham.me/concordance_plot/
