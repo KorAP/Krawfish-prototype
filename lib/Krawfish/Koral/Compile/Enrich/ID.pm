014ec1b5e3bc24f75ee7b7e82d9aec9a2bbf6613
Akron <nils@diewald-online.de>
2020-04-22 12:39:45 +0200

    Enrich matches with specific match IDs
    Change-Id: Ib78fd1195063b15836a3293408cd8c2a77f96ecf

---
+++ lib/Krawfish/Koral/Compile/Enrich/ID.pm
@@ -0,0 +1,5 @@
+# Enrich a snippet by ID, which can either
+# be the normal match identifier or a persistent
+# identifier with character offsets. In which case
+# it may be better to be created based on snippet
+# information if the snippet exist.
